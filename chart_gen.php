92d1d3ef12ac8651ab747c836edd86b25a3b091b
Mohammed Rakibur Rahman Ranak <47697931+rakibur992@users.noreply.github.com>
2019-04-04 21:14:44 +0600

    new
    as

--- chart_gen.php
+++ chart_gen.php
@@ -99,6 +99,29 @@ while ($row = mysqli_fetch_assoc($result)) {
     $u_expense[$i]=$row["Total_Expense"];
     $i+=1;
 }
+//Generating Student distribution charts
+$sql16 = "SELECT  Female_Student,Undergraduate_pass,Undergraduate_Hons,
+Undergraduate_Passed_Student_Normal,Undergraduate_Passed_Student_Hons,Undergraduate_Passed_Student_Eng_tech,
+Graduate_Passed_Student_Normal,Graduate_Passed_Student_Eng_tech,Student_Proverty,Student_Freedom_Fighter
+ FROM university u,student_distribution s WHERE u.UYear='2017' AND u.Uid_PK=s.uID_PK AND u.UYear=s.year_PK  AND u.University_Name='North South University'";
+$result = mysqli_query($conn, $sql16);
+
+if (mysqli_num_rows($result) > 0) {
+
+    // output data of one row
+    $row = mysqli_fetch_assoc($result);
+
+    $data_pie_exp1[0]=$row["Female_Student"] ;
+    $data_pie_exp1[1]=$row["Undergraduate_pass"] ;
+    $data_pie_exp1[2]=$row["Undergraduate_Hons"] ;
+    $data_pie_exp1[3]=$row["Undergraduate_Passed_Student_Normal"] ;
+    $data_pie_exp1[4]=$row["Undergraduate_Passed_Student_Hons"] ;
+    $data_pie_exp1[5]=$row["Undergraduate_Passed_Student_Eng_tech"] ;
+    $data_pie_exp1[6]=$row["Graduate_Passed_Student_Normal"] ;
+    $data_pie_exp1[7]=$row["Graduate_Passed_Student_Eng_tech"] ;
+    $data_pie_exp1[8]=$row["Student_Proverty"] ;
+    $data_pie_exp1[9]=$row["Student_Freedom_Fighter"] ;
+}
 
 
 
