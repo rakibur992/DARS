dd99a48ada06350f0d8c551c19e598a5928e9cbc
sajid9505 <42285255+sajid9505@users.noreply.github.com>
2019-04-11 03:59:57 +0600

    changed names

--- home.php
+++ home.php
@@ -82,14 +82,14 @@
                                 <li class="divider"></li>
                                <li><a class="collapsed" data-toggle="collapse" href="#togglePages"><i class="menu-icon icon-paste">
                                 </i><i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right">
-                                </i>University Information</a>
+                                </i>Information Input</a>
                                     <ul id="togglePages" class="collapse unstyled">
-                                        <li><a href="UniversityInfo.php"><i class="menu-icon icon-paste"></i>General Information </a></li>
+                                        <li><a href="UniversityInfo.php"><i class="menu-icon icon-paste"></i>University Information </a></li>
                                         <li><a href="TeacherInfo.php"><i class="menu-icon icon-paste"></i>Teacher Distribution </a></li>
                                        <li><a href="StudentInfo.php"><i class="menu-icon icon-paste"></i>Student Distribution </a></li>
                                        <li><a href="ResearchPublications.php"><i class="menu-icon icon-paste"></i>Research and Publications </a></li>
                                        <li><a href="Finances.php"><i class="menu-icon icon-paste"></i>Finances </a></li>
-                                       <li><a href="Courses.php"><i class="menu-icon icon-paste"></i>Courses </a></li>
+                                       <li><a href="Courses.php"><i class="menu-icon icon-paste"></i>Subjects </a></li>
                                     </ul>
                             </li>
                                 <li><a href="table.php"><i class="menu-icon icon-table"></i>Tables </a></li>
